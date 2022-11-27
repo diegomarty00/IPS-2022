35e6d4d4665e67a15fe7dd3410eb8eca6f0df1af
Diego Martínez <diegomarty1200@gmail.com>
2022-11-27 21:26:31 +0100

    Cambios que estaban mal para el main

--- IPS2022-PL1-2/src/persistencia/cita/CitaRecord.java
+++ IPS2022-PL1-2/src/persistencia/cita/CitaRecord.java
@@ -9,40 +9,32 @@ import util.BusinessException;
 
 public class CitaRecord {
 
-	
-	public String idCita;
-	public String dniPaciente;
-	public int idHistorial;
-	public boolean urgente;
-	public LocalTime horaEntradaEstimada;
-	public LocalTime horaSalidaEstimada;
-	public String pacienteAcudido;
-	public LocalTime horaEntradaReal;
-	public LocalTime horaSalidaReal;
-	public LocalDate fecha;
-	public String correoPaciente; 
-	public String telefonoPaciente; 
-	public String lugar;
-	public String otros;
-	public boolean prioritario;
-	public PacienteRecord pacienteAsociado;
-	public boolean confirmada;
+    public String idCita;
+    public String dniPaciente;
+    public int idHistorial;
+    public boolean urgente;
+    public LocalTime horaEntradaEstimada;
+    public LocalTime horaSalidaEstimada;
+    public String pacienteAcudido;
+    public LocalTime horaEntradaReal;
+    public LocalTime horaSalidaReal;
+    public LocalDate fecha;
+    public String correoPaciente;
+    public String telefonoPaciente;
+    public String lugar;
+    public String otros;
+    public boolean prioritario;
+    public PacienteRecord pacienteAsociado;
+    public boolean confirmada;
 
-	public PacienteRecord getPacienteAsociado() {
-		try {
-			if (pacienteAsociado!=null)
-				return pacienteAsociado;
-			return BusinessFactory.forPacienteService().getByDni(dniPaciente).get();
-		} catch (BusinessException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-	
-	public String toString() {
-		PacienteRecord pacienteAsociado = getPacienteAsociado();
-		return pacienteAsociado.getNombre()+" "+pacienteAsociado.getApellidos()+" "+horaEntradaEstimada.toString()+" - "+horaSalidaEstimada.toString();
-
+    public PacienteRecord getPacienteAsociado() {
+	try {
+	    if (pacienteAsociado != null)
+		return pacienteAsociado;
+	    return BusinessFactory.forPacienteService().getByDni(dniPaciente)
+		    .get();
+	} catch (BusinessException e) {
+	    e.printStackTrace();
 	}
 	return null;
     }
--- IPS2022-PL1-2/src/persistencia/paciente/impl/PacienteGatewayImpl.java
+++ IPS2022-PL1-2/src/persistencia/paciente/impl/PacienteGatewayImpl.java
@@ -19,12 +19,12 @@ import util.jdbc.Jdbc;
 
 public class PacienteGatewayImpl implements PacienteGateway {
 
-	private static final String PACIENTE_DNI = "SELECT * from PACIENTE where DNI = ?";
-	private static final String HISTORIAL_DNI = "SELECT * from HISTORIAL where DNIPACIENTE = ?";
-	private static final String VACUNAR = "INSERT INTO VACUNA values (?,?,?,?,?,?,?,?)";
-	private static final String FIND_VACUNAS_REALIZADAS = "SELECT * FROM VACUNA WHERE IDHISTORIAL = ? ORDER BY FECHAAPROXIMADA ASC";
-	private static final String CHECK_IF_EXISTS_VACUNA = "SELECT * FROM VACUNA WHERE IDVACUNA = ?";
-	private static final String UPDATE_VACUNA = "UPDATE VACUNA SET IDCITA = ?, FECHAREAL = ?, HORA = ?, DOSIS = ?, REFUERZO = ? WHERE IDVACUNA = ?";
+    private static final String PACIENTE_DNI = "SELECT * from PACIENTE where DNI = ?";
+    private static final String HISTORIAL_DNI = "SELECT * from HISTORIAL where DNIPACIENTE = ?";
+    private static final String VACUNAR = "INSERT INTO VACUNA values (?,?,?,?,?,?,?,?)";
+    private static final String FIND_VACUNAS_REALIZADAS = "SELECT * FROM VACUNA WHERE IDHISTORIAL = ? ORDER BY FECHAAPROXIMADA ASC";
+    private static final String CHECK_IF_EXISTS_VACUNA = "SELECT * FROM VACUNA WHERE IDVACUNA = ?";
+    private static final String UPDATE_VACUNA = "UPDATE VACUNA SET IDCITA = ?, FECHAREAL = ?, HORA = ?, DOSIS = ?, REFUERZO = ? WHERE IDVACUNA = ?";
 
     @Override
     public void add(PacienteRecord t) {
@@ -91,134 +91,131 @@ public class PacienteGatewayImpl implements PacienteGateway {
 	}
     }
 
-    
-	@Override
-	public HistorialRecord getHistorial(String dniPaciente) {
-		Connection c = null;
-		PreparedStatement pst = null;
-		ResultSet rs = null;
-		
-		try {
-			c = Jdbc.getConnection();
-			
-			pst = c.prepareStatement(HISTORIAL_DNI);
-			pst.setString(1, dniPaciente);
+    @Override
+    public HistorialRecord getHistorial(String dniPaciente) {
+	Connection c = null;
+	PreparedStatement pst = null;
+	ResultSet rs = null;
 
-			pst.execute();
-			
-			rs = pst.executeQuery();
-			
-			return RecordAssembler.toHistorialRecord(rs).get();
-		} catch (SQLException e) {
-			throw new PersistenceException(e);
-		} finally {
-			Jdbc.close(rs, pst);
-		}
+	try {
+	    c = Jdbc.getConnection();
+
+	    pst = c.prepareStatement(HISTORIAL_DNI);
+	    pst.setString(1, dniPaciente);
+
+	    pst.execute();
+
+	    rs = pst.executeQuery();
+
+	    return RecordAssembler.toHistorialRecord(rs).get();
+	} catch (SQLException e) {
+	    throw new PersistenceException(e);
+	} finally {
+	    Jdbc.close(rs, pst);
 	}
     }
 
-	
-	@Override
-	public void vacunar(VacunaRecord vacuna) {
-		Connection c = null;
-		PreparedStatement pst = null;
-		
-		try {
-			c = Jdbc.getConnection();
-			
-			if (existsVacuna(vacuna.getIdVacuna())) {
-				updateVacuna(vacuna);
-				return;
-			}
-			
-			pst = c.prepareStatement(VACUNAR);
-			pst.setInt(1, vacuna.getIdVacuna());
-			pst.setInt(2, vacuna.getIdHistorial());
-			pst.setString(3, vacuna.getIdCita());
-			pst.setDate(4, Date.valueOf(vacuna.getFechaReal()));
-			pst.setDate(5, Date.valueOf(vacuna.getFechaAproximada()));
-			pst.setTime(6, Time.valueOf(vacuna.getHora()));
-			pst.setString(7, vacuna.getDosis());
-			pst.setBoolean(8, vacuna.isRefuerzo());
-			
-			pst.execute();
-			
-		} catch (SQLException e) {
-			throw new PersistenceException(e);
-		} finally {
-			Jdbc.close(pst);
-		}
+    @Override
+    public void vacunar(VacunaRecord vacuna) {
+	Connection c = null;
+	PreparedStatement pst = null;
+
+	try {
+	    c = Jdbc.getConnection();
+
+	    if (existsVacuna(vacuna.getIdVacuna())) {
+		updateVacuna(vacuna);
+		return;
+	    }
+
+	    pst = c.prepareStatement(VACUNAR);
+	    pst.setInt(1, vacuna.getIdVacuna());
+	    pst.setInt(2, vacuna.getIdHistorial());
+	    pst.setString(3, vacuna.getIdCita());
+	    pst.setDate(4, Date.valueOf(vacuna.getFechaReal()));
+	    pst.setDate(5, Date.valueOf(vacuna.getFechaAproximada()));
+	    pst.setTime(6, Time.valueOf(vacuna.getHora()));
+	    pst.setString(7, vacuna.getDosis());
+	    pst.setBoolean(8, vacuna.isRefuerzo());
+
+	    pst.execute();
+
+	} catch (SQLException e) {
+	    throw new PersistenceException(e);
+	} finally {
+	    Jdbc.close(pst);
 	}
-	
-	private void updateVacuna(VacunaRecord vacuna) {
-		Connection c = null;
-		PreparedStatement pst = null;
-		
-		try {
-			c = Jdbc.getConnection();
-			
-			pst = c.prepareStatement(UPDATE_VACUNA);
-			
-			pst.setString(1, vacuna.getIdCita());
-			pst.setDate(2, Date.valueOf(vacuna.getFechaReal()));
-			pst.setTime(3, Time.valueOf(vacuna.getHora()));
-			pst.setString(4, vacuna.getDosis());
-			pst.setBoolean(5, vacuna.isRefuerzo());
-			pst.setInt(6, vacuna.getIdVacuna());
-			
-			pst.execute();
-			
-		} catch (SQLException e) {
-			throw new PersistenceException(e);
-		} finally {
-			Jdbc.close(pst);
-		}
+    }
+
+    private void updateVacuna(VacunaRecord vacuna) {
+	Connection c = null;
+	PreparedStatement pst = null;
+
+	try {
+	    c = Jdbc.getConnection();
+
+	    pst = c.prepareStatement(UPDATE_VACUNA);
+
+	    pst.setString(1, vacuna.getIdCita());
+	    pst.setDate(2, Date.valueOf(vacuna.getFechaReal()));
+	    pst.setTime(3, Time.valueOf(vacuna.getHora()));
+	    pst.setString(4, vacuna.getDosis());
+	    pst.setBoolean(5, vacuna.isRefuerzo());
+	    pst.setInt(6, vacuna.getIdVacuna());
+
+	    pst.execute();
+
+	} catch (SQLException e) {
+	    throw new PersistenceException(e);
+	} finally {
+	    Jdbc.close(pst);
+	}
+    }
+
+    private boolean existsVacuna(int idVacuna) {
+	Connection c = null;
+	PreparedStatement pst = null;
+	ResultSet rs = null;
+
+	try {
+	    c = Jdbc.getConnection();
+
+	    pst = c.prepareStatement(CHECK_IF_EXISTS_VACUNA);
+	    pst.setInt(1, idVacuna);
+
+	    rs = pst.executeQuery();
+
+	    return rs.isBeforeFirst();
+
+	} catch (SQLException e) {
+	    throw new PersistenceException(e);
+	} finally {
+	    Jdbc.close(rs, pst);
+	}
+    }
+
+    @Override
+    public List<VacunaRecord> getVacunas(int idHistorial) {
+	Connection c = null;
+	PreparedStatement pst = null;
+	ResultSet rs = null;
+
+	try {
+	    c = Jdbc.getConnection();
+
+	    pst = c.prepareStatement(FIND_VACUNAS_REALIZADAS);
+	    pst.setInt(1, idHistorial);
+
+	    rs = pst.executeQuery();
+
+	    return RecordAssembler.toVacunaList(rs);
+
+	} catch (SQLException e) {
+	    throw new PersistenceException(e);
+	} finally {
+	    Jdbc.close(rs, pst);
 	}
 
-	private boolean existsVacuna(int idVacuna) {
-		Connection c = null;
-		PreparedStatement pst = null;
-		ResultSet rs = null;
-
-		try {
-			c = Jdbc.getConnection();
-			
-			pst = c.prepareStatement(CHECK_IF_EXISTS_VACUNA);
-			pst.setInt(1, idVacuna);
-			
-			rs = pst.executeQuery();
-			
-			return rs.isBeforeFirst();
-
-		} catch (SQLException e) {
-			throw new PersistenceException(e);
-		} finally {
-			Jdbc.close(rs, pst);
-		}
-	}
-
-	@Override
-	public List<VacunaRecord> getVacunas(int idHistorial) { 
-		Connection c = null;
-		PreparedStatement pst = null;
-		ResultSet rs = null;
-
-		try {
-			c = Jdbc.getConnection();
-			
-			pst = c.prepareStatement(FIND_VACUNAS_REALIZADAS);
-			pst.setInt(1, idHistorial);
-			
-			rs = pst.executeQuery();
-			
-			return RecordAssembler.toVacunaList(rs);
-
-		} catch (SQLException e) {
-			throw new PersistenceException(e);
-		} finally {
-			Jdbc.close(rs, pst);
-		}
-
-	}
+    }
 
 }
