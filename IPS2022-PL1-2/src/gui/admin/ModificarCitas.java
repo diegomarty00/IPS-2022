b7889954fd9fa4cb1c070a32b02dc21a155a381e
Felipe-Garcia-Gomez <uo285267@uniovi.es>
2022-12-11 14:55:41 +0100

    Modificación de un modelo

--- IPS2022-PL1-2/src/gui/admin/ModificarCitas.java
+++ IPS2022-PL1-2/src/gui/admin/ModificarCitas.java
@@ -635,7 +635,7 @@ public class ModificarCitas extends JFrame {
 	    private JComboBox getCbMes() {
 		if (cbMes == null) {
 		    cbMes = new JComboBox();
-		    cbMes.setModel(new DefaultComboBoxModel(new String[] {"01", "02", "03", "04", "05", "07", "08", "09", "10", "11", "12"}));
+		    cbMes.setModel(new DefaultComboBoxModel(new String[] {"01", "02", "03", "04", "05", "06","07", "08", "09", "10", "11", "12"}));
 		    cbMes.setSelectedIndex(cita.fecha.getMonthValue()-1);
 		}
 		return cbMes;
